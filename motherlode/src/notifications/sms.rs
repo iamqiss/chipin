bdf92670be29bc0fd16763481ffe1dd3d88ce331
Neo Qiss <ngomaneneo24@gmail.com>
2026-04-13 05:35:00 +0200

    Update sms.rs

--- motherlode/src/notifications/sms.rs
+++ motherlode/src/notifications/sms.rs
@@ -1,1 +1,7 @@
+use crate::{config::Config, errors::AppResult};
 
+pub async fn send(phone: &str, message: &str, config: &Config) -> AppResult<()> {
+    // TODO: implement Vonage/Infobip SMS delivery
+    tracing::info!(phone = %phone, "SMS stub: {}", message);
+    Ok(())
+}
